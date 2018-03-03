a1ab8667bfbb2a248e2efaac5db504ae5c5d4a98
atsushi130 <atsushi_main@outlook.jp>
2018-03-03 17:37:51 +0900

    define: Outputable protocol

--- Connectable/Outputable.swift
+++ Connectable/Outputable.swift
@@ -7,3 +7,26 @@
 //
 
 import Foundation
+
+public struct OutputSpace<Definer> {
+    let definer: Definer
+    init(_ definer: Definer) {
+        self.definer = definer
+    }
+}
+
+public protocol Outputable {
+    static var out: OutputSpace<Self>.Type { get }
+    var out: OutputSpace<Self> { get }
+}
+
+extension Outputable {
+    
+    public static var out: OutputSpace<Self>.Type {
+        return OutputSpace<Self>.self
+    }
+    
+    public var out: OutputSpace<Self> {
+        return OutputSpace(self)
+    }
+}
