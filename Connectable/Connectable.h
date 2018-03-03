f58320432e1ebc368d5f5f076d31e9bea01ae954
atsushi130 <atsushi_main@outlook.jp>
2018-03-03 17:28:27 +0900

    Initial Commit

---
+++ Connectable/Connectable.h
@@ -0,0 +1,19 @@
+//
+//  Connectable.h
+//  Connectable
+//
+//  Created by Atsushi Miyake on 2018/03/03.
+//  Copyright © 2018年 Atsushi Miyake. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Connectable.
+FOUNDATION_EXPORT double ConnectableVersionNumber;
+
+//! Project version string for Connectable.
+FOUNDATION_EXPORT const unsigned char ConnectableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Connectable/PublicHeader.h>
+
+
