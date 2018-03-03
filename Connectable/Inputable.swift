c0d792533aa8c7eb5e83a5b522f1f643873d42b4
atsushi130 <atsushi_main@outlook.jp>
2018-03-03 17:37:41 +0900

    define: Inputable protocol

--- Connectable/Inputable.swift
+++ Connectable/Inputable.swift
@@ -7,3 +7,26 @@
 //
 
 import Foundation
+
+public struct InputSpace<Definer> {
+    let definer: Definer
+    init(_ definer: Definer) {
+        self.definer = definer
+    }
+}
+
+public protocol Inputable {
+    static var `in`: InputSpace<Self>.Type { get }
+    var `in`: InputSpace<Self> { get }
+}
+
+extension Inputable {
+    
+    public static var `in`: InputSpace<Self>.Type {
+        return InputSpace<Self>.self
+    }
+    
+    public var `in`: InputSpace<Self> {
+        return InputSpace(self)
+    }
+}
